e893c974ae2d26fae3018f99aa6c63009748cca4
SeongjaePark <gndan4@gmail.com>
2021-06-08 15:23:55 +0900

    feat: Make EventsOrganizedByUserController & Register it in events module

--- src/events/event.service.ts
+++ src/events/event.service.ts
@@ -4,7 +4,7 @@ import { User } from 'src/auth/user.entity';
 import { paginate, PaginateOptions } from 'src/pagination/paginator';
 import { DeleteResult, Repository } from 'typeorm';
 import { AttendeeAnswerEnum } from './attendee.entity';
-import { Event } from './event.entity';
+import { Event, PaginatedEvents } from './event.entity';
 import { CreateEventDto } from './input/create-event.dto';
 import { ListEvents, WhenEventFilter } from './input/list.events';
 import { UpdateEventDto } from './input/update-event.dto';
@@ -88,7 +88,7 @@ export class EventsService {
   public async getEventsWithAttendeeCountFilteredPaginated(
     filter: ListEvents,
     PaginateOptions: PaginateOptions,
-  ) {
+  ): Promise<PaginatedEvents> {
     return await paginate(
       await this.getEventsWithAttendeeCountFiltered(filter),
       PaginateOptions,
@@ -131,5 +131,21 @@ export class EventsService {
       .delete()
       .where('id = :id', { id })
       .execute();
+  }
+
+  public async getEventsOrganizedByUserIdPaginated(
+    userId: number,
+    paginateOptions: PaginateOptions,
+  ): Promise<PaginatedEvents> {
+    return paginate<Event>(
+      this.getEventsOrganizedByUserIdQuery(userId),
+      paginateOptions,
+    );
+  }
+
+  public getEventsOrganizedByUserIdQuery(userId: number) {
+    return this.getEventsBaseQuery().where('e.organizer_id = :userId', {
+      userId,
+    });
   }
 }
--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -5,11 +5,16 @@ import { AttendeesService } from './attendee.service';
 import { AttendeesController } from './event-attendees.controller';
 import { Event } from './event.entity';
 import { EventsService } from './event.service';
+import { EventsOrganizedByUserController } from './events-organized-by-user.controller';
 import { EventsController } from './events.contoller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Event, Attendee])],
-  controllers: [EventsController, AttendeesController],
+  controllers: [
+    EventsController,
+    AttendeesController,
+    EventsOrganizedByUserController,
+  ],
   providers: [EventsService, AttendeesService],
 })
 export class EventsModule {}
